a8334dd785f0162a192f0ef4eea7143422435e74
ThamiduChamod <thamiduchamod748@gmail.com>
2026-04-18 12:28:01 +0530

    update

--- sensor-service/src/main/java/lk/ijse/sensorservice/client/AutomationClient.java
+++ sensor-service/src/main/java/lk/ijse/sensorservice/client/AutomationClient.java
@@ -1,6 +1,7 @@
 package lk.ijse.sensorservice.client;
 
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
--- sensor-service/src/main/java/lk/ijse/sensorservice/client/IoTClient.java
+++ sensor-service/src/main/java/lk/ijse/sensorservice/client/IoTClient.java
@@ -1,5 +1,6 @@
 package lk.ijse.sensorservice.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
